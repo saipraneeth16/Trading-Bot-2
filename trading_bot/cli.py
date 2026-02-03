cade66a83981d364d760f741918d4e52607cf22c
saipraneeth16 <74300864+saipraneeth16@users.noreply.github.com>
2026-02-03 13:51:27 +0530

    Add CLI for Binance Futures Trading Bot

---
+++ trading_bot/cli.py
@@ -0,0 +1,40 @@
+import argparse
+from bot.client import BinanceFuturesClient
+from bot.orders import place_order
+
+def main():
+    parser = argparse.ArgumentParser(description="Binance Futures Trading Bot")
+
+    parser.add_argument("--symbol", required=True)
+    parser.add_argument("--side", required=True)
+    parser.add_argument("--type", required=True)
+    parser.add_argument("--quantity", type=float, required=True)
+    parser.add_argument("--price", type=float)
+
+    args = parser.parse_args()
+
+    client = BinanceFuturesClient()
+
+    try:
+        response = place_order(
+            client=client,
+            symbol=args.symbol,
+            side=args.side,
+            order_type=args.type,
+            quantity=args.quantity,
+            price=args.price
+        )
+
+        print("✅ Order placed successfully")
+        print({
+            "orderId": response.get("orderId"),
+            "status": response.get("status"),
+            "executedQty": response.get("executedQty"),
+            "avgPrice": response.get("avgPrice")
+        })
+
+    except Exception as e:
+        print("❌ Order failed:", str(e))
+
+if __name__ == "__main__":
+    main()
